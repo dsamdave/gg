1ed92b1145d6701bf3440b647b3714394cb73187
David Sampson <Davidchosen33@gmail.com>
2024-03-05 21:07:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+
+
+// Programming language
+
+// Only programing for the web
+
+// ES
+// ECMAScript
+// Es5 or ES6
+// ES2014 ES2015
+
+
+// Variables
+// Strings
+// Numbers 0 11234567890
+// Boolean
+// Undefined
+// Null
+
+// Strings ""
+
+// Declaring variables
+
+// Can you hear me?
+
+// Declare 5 string, number, booleans, null & undefined
+
+// Console log them different
+
+// Variable should be atleast 5 syllabuls
+
+
+var fff = "David"
+
+var myAge = 120
+
+var mySisterInlawBrotherCousinName = "Ruth"
+
+var isMarried = false
+var isFinished = true
+
+
+var dd = null
+
+var cc 
+
+
+// Camel Casing
+
+// console.log(fff)
+
+// console.log(myAge)
+
+
+// String and String Methods, properties
+
+
+const myFirstName = "Davidd"
+
+const myLastName = "Sampson"
+
+const myNewAge = 76
+
+console.log(myFirstName.endsWith("f"))
+console.log(myFirstName.startsWith("D"))
+console.log(myFirstName.includes("5"))
+
+console.log(myFirstName.length)
+
+console.log(myFirstName.trim().length)
+
+// String Conacatenation  joining strings in a sentence
+
+const message = "My name is " + myFirstName + " " + myLastName + " and I am " + myNewAge + " years old"
+
+const msg = `My name is ${myFirstName} ${myLastName} and I am ${myNewAge} years old`
+
+console.log(message)
+
+console.log(msg)
+
+
+// Numbers + - * ** / % < > <= >=
+
+
+const a = Math.PI
+
+const b = 2
+const c = 58
+
+const d = (a * b)**3 + (c**5 - a) / 6**7
+
+const aa = Math.floor(Math.random() * 10000) 
+
+const id = `Trans-${aa}`
+
+console.log(id)
+
+
+var g = "ghkdftydft"
+
+const hh = 2
+
+ let j = 7
+
+  j = 8
