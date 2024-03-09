6db497079c2b24ed4c3584284fcbcdb5519a7b7c
Sampson David <davidchosen33@gmail.com>
2024-03-09 19:10:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,23 +61,18 @@ const myLastName = "Sampson"
 
 const myNewAge = 76
 
-console.log(myFirstName.endsWith("f"))
-console.log(myFirstName.startsWith("D"))
-console.log(myFirstName.includes("5"))
 
-console.log(myFirstName.length)
 
-console.log(myFirstName.trim().length)
 
 // String Conacatenation  joining strings in a sentence
 
+// Concatenation ES5
 const message = "My name is " + myFirstName + " " + myLastName + " and I am " + myNewAge + " years old"
 
+// string literal ES6
 const msg = `My name is ${myFirstName} ${myLastName} and I am ${myNewAge} years old`
 
-console.log(message)
 
-console.log(msg)
 
 
 // Numbers + - * ** / % < > <= >=
@@ -94,7 +89,6 @@ const aa = Math.floor(Math.random() * 10000)
 
 const id = `Trans-${aa}`
 
-console.log(id)
 
 
 var g = "ghkdftydft"
@@ -104,11 +98,6 @@ const hh = 2
  let j = 7
 
   j = 8
-
-
-
-
-
 
 
 
@@ -160,10 +149,209 @@ const person = {
 
 }
 
-console.log(kk[3])
 
-console.log(person.firstName, person.age)
 
-`Thes are m,y favopr {4} {2} {5}`
 
-`{}{}{}{}{}`
+
+const studentss = [
+    ["ghvgv", "hjvghvyu", "uyjhmvuyfv"],
+    [
+        1, 
+        4, 
+        7, 
+        67,
+        [ 76, 98, 0, 434]
+
+    ],
+    [12, 56, true, "jhfgfvb"]
+]
+
+// console.log(students[1][4][3])
+
+
+
+
+const students = [
+    {
+        firstName: "David",
+        lastName: "Sampson",
+        location: "PH",
+        Score: 66
+    },
+    {
+        firstName: "Immaculate",
+        lastName: "Daniel",
+        location: "Imo",
+        Score: 70
+    },
+    {
+        firstName: "Lucy",
+        lastName: "Elijah",
+        location: "Delta",
+        Score: 80
+    },
+]
+
+// console.log(students[2].firstName)
+
+const studentArray = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      phoneNumber: "123-456-7890",
+      score: 85,
+      address: "123 Main St, Cityville"
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      email: "jane.smith@example.com",
+      phoneNumber: "987-654-3210",
+      score: 92,
+      address: "456 Oak St, Townsville"
+    },
+  ];
+  
+
+//   console.log(studentArray[1].emaillg
+
+const user = {
+    consultantDetails: {
+        background: {
+            expertise: [
+                "TTTTT"
+            ],
+            disciplines: [
+                "Chemistry"
+            ],
+            industries: [
+                "Software"
+            ],
+            languages: [
+                "English",
+                "French"
+            ]
+        },
+        education: [
+            {
+                title: "string",
+               description: "string",
+                year: "string"
+            },
+            {
+                title: "string",
+               description: "string",
+                year: "string"
+            },
+            {
+                title: "string",
+               description: "string",
+                year: "string"
+            },
+            {
+                title: "string",
+               description: "string",
+                year: "string"
+            }
+        ],
+        experience: [
+            {
+                date: {
+                    from: "December 2024",
+                    to: "January 2025"
+                },
+                title: "string",
+                description: "string"
+            },
+            {
+                date: {
+                    from: "December 2024",
+                    to: "January 2025"
+                },
+                title: "string",
+                description: "string"
+            }
+        ]
+    },
+    schoolAxisID: "schA-fa300e8e",
+    email: "virtualsavvy.nigeria@gmail.com",
+    firstName: "david",
+    lastName: "sampson",
+    role: "consultant",
+    avatar: "https://res.cloudinary.com/virtualsavvyng/image/upload/v1681556292/realties/user1_xhiugc.png",
+    disabled: false,
+    active: true,
+    id: "65bc14cdafca680ca1a2c04a"
+}
+
+// JSON
+
+// console.log(user.consultantDetails.background.languages[0])
+
+// firstName,
+// Email,
+// 3rd Education object
+
+// const message = `first name is ${hvhvh.jgygf}`
+
+
+
+
+
+// let y = 3
+
+// y--
+// y--
+// y--
+// y--
+// y--
+
+// console.log(y)
+
+
+// Loops
+
+// While loop
+// ForLoop
+
+
+
+let x = 0
+
+
+while( x < 10 ){
+
+    console.log("Good morning")
+
+    x++
+}
+
+
+
+// whem to start,
+// when to stop
+// increase the variable used
+
+for( let x = 5; x < 11; x++){
+    console.log("Were you there?")
+}
+
+
+const spices = ["gfgj", 22, true, "gfgc", 666, "jhvghf", 54555]
+
+
+for(let p = 0; p < spices.length; p++){
+    console.log(spices[p])
+}
+
+const ggg = "David"
+
+for(let y = 0; y < ggg.length; y++){
+    console.log(ggg[y])
+}
+
+// Write while,
+// Write forloop
+// Using forLoop print out the names of students in ypur student array
+
